031fdedcd80bf78c5501bb82c7e02348beabb560
arun <>
2022-01-07 01:02:18 +0100

    Integrate theme

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .sass('resources/scss/app.scss', 'public/css')
+    .options({processCssUrls: false})
+    .sourceMaps(true, 'source-map')
+    .version();
